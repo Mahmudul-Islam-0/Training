ec7c750dcaf98cc15fdaadf64369c857197f5c0b
Mahmudul Islam <106462850+Mahmudul-Islam-0@users.noreply.github.com>
2024-10-26 15:34:29 +0600

    Update EmployeeController.cs

--- Training.MVC/Controllers/EmployeeController.cs
+++ Training.MVC/Controllers/EmployeeController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Training.DataAcces.Repository;
-using Training.Models.DTO; // Make sure to include the DTO namespace
-using Training.Models.Entity; // Assuming you still have an Employee entity
-
+using Training.Models.DTO; 
+using Training.Models.Entity; 
 namespace Training.MVC.Controllers
 {
     public class EmployeeController : Controller
@@ -14,12 +13,10 @@ namespace Training.MVC.Controllers
             this.unitOfWork = unitOfWork;
         }
 
-        // Action to view the list of employees
         public async Task<IActionResult> EmployeeView()
         {
             ViewBag.Department = await unitOfWork.DepartmentRepo.ToListAsync();
 
-            // Fetch the employees and map them to EmpListDTO
             var employees = await unitOfWork.EmployeeRepo.GetAll();
             var employeeDtos = employees.Select(emp => new Employee
             {
@@ -31,23 +28,20 @@ namespace Training.MVC.Controllers
                 DeptId = emp.DeptId
             }).ToList();
 
-            return View(employeeDtos); // Pass the list of DTOs to the view
+            return View(employeeDtos); 
         }
 
-        // Action to show the form for saving a new employee
         public IActionResult EmployeeSave()
         {
-            return View(); // Return the empty form for a new employee
+            return View(); 
         }
 
-        // Action to handle the form submission for saving a new employee
         [HttpPost]
         public async Task<IActionResult> EmployeeSave(Employee employeeDto)
         {
 
             if (ModelState.IsValid)
             {
-                // Map DTO to Entity (assuming you have an Employee entity)
                 var employee = new Employee
                 {
                     Id = employeeDto.Id,
@@ -59,10 +53,10 @@ namespace Training.MVC.Controllers
                 };
 
                 await unitOfWork.EmployeeRepo.AddAsync(employee);
-                await unitOfWork.EmployeeRepo.Save(); // Save changes
-                return RedirectToAction("EmployeeView"); // Redirect to the employee list after saving
+                await unitOfWork.EmployeeRepo.Save(); 
+                return RedirectToAction("EmployeeView"); 
             }
-            return View(employeeDto); // Return the same view with the model if validation fails
+            return View(employeeDto); 
         }
     }
 }
